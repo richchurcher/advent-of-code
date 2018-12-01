e75d4ad49561969e6b128a38b746b6c77659f194
Rich Churcher <rich.churcher@gmail.com>
2018-12-01 19:39:43 +1300

    Simplify, add input

--- src/day1.rs
+++ src/day1.rs
@@ -3,13 +3,3 @@ pub fn calibrate_device (input: &str) {
     return 0;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn calculates_frequency_for_three_values () {
-        assert_eq(2, calibrate_device("1, -1, 2"));
-    }
-}
-
